37a42fc2e7698e7c01135c43f96537e2954c78bf
CN-Liquid <adhikariaakash90@gmail.com>
2025-06-28 21:52:05 +0545

    Added pragma once at the top of the matrix header

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,23 +1,23 @@
-class matrix
-{
-    public:
-    int rows,columns;
-    double **data;
+#pragma once
+class matrix {
+public:
+  int rows, columns;
+  double **data;
 
-    matrix(int,int);
-    matrix(const matrix& other);
-    ~matrix();
+  matrix(int, int);
+  matrix(const matrix &other);
+  ~matrix();
 
-    void fill(double value);
-    matrix transpose()const;
-    matrix operator+(const matrix &other) const;
-    matrix operator*(const matrix &other) const;
-    double sigmoid(double value);
-    void applyfunction(double (*f)(double),double value);
+  void fill(double value);
+  matrix transpose() const;
+  matrix operator+(const matrix &other) const;
+  matrix operator*(const matrix &other) const;
+  double sigmoid(double value);
+  void applyfunction(double (*f)(double), double value);
 
-    void display();
-    
-    private:
-    void allocatememory();
-    void deallocatememory();
+  void display();
+
+private:
+  void allocatememory();
+  void deallocatememory();
 };
