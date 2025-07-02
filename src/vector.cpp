67d5aae47db9697c95fd95db0899bcd1d483d016
ayush69-cyber <ayushkhanal161@gmail.com>
2025-07-02 22:46:31 +0545

    Defined New Operations

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,4 +1,5 @@
 #include "vector.hpp"
+#include<cmath>
 #include<iostream>
 Vector::Vector(int n):matrix(n,1){}
 Vector::Vector(matrix mat):matrix(mat){}
@@ -13,4 +14,37 @@ double Vector :: dot(Vector& other){
         result += ((data[i][0])*other.data[i][0]);
     }
     return result;
+}
+double Vector:: magnitude()
+{
+    double result = 0;
+    for(int i = 0 ; i<rows ; i++)
+    {
+        result += (data[i][0] * data[i][0]);
+    }
+    return pow(result,0.5);
+}
+void Vector:: normalize()
+{
+    double mag = magnitude();
+    for(int i = 0; i<rows ; i++)
+    {
+        data[i][0]/=mag;
+    }
+}
+Vector Vector::projectonto(Vector& other)
+{
+    
+    Vector temp(rows);
+    double scalar = dot(other)/(other.magnitude()*other.magnitude());
+    for(int i = 0; i<rows ; i++)
+    {
+        temp.data[i][0] = other.data[i][0] * scalar;
+        
+    }
+    return temp;
+}
+double Vector::angle_with(Vector& other)
+{
+    return acos(dot(other)/(magnitude()*other.magnitude()));
 }
