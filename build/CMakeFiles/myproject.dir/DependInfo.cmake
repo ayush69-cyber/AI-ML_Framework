86920c3f9f2e119095d752651a8e70261a9cc3e0
ayush69-cyber <ayushkhanal161@gmail.com>
2025-07-11 09:32:39 +0545

    some changes

--- build/CMakeFiles/myproject.dir/DependInfo.cmake
+++ build/CMakeFiles/myproject.dir/DependInfo.cmake
@@ -8,11 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/Ayush Khanal/AI-ML_Framework/src/Network.cpp" "CMakeFiles/myproject.dir/src/Network.cpp.obj" "gcc" "CMakeFiles/myproject.dir/src/Network.cpp.obj.d"
-  "C:/Users/Ayush Khanal/AI-ML_Framework/src/lin_regression.cpp" "CMakeFiles/myproject.dir/src/lin_regression.cpp.obj" "gcc" "CMakeFiles/myproject.dir/src/lin_regression.cpp.obj.d"
-  "C:/Users/Ayush Khanal/AI-ML_Framework/src/main.cpp" "CMakeFiles/myproject.dir/src/main.cpp.obj" "gcc" "CMakeFiles/myproject.dir/src/main.cpp.obj.d"
-  "C:/Users/Ayush Khanal/AI-ML_Framework/src/matrix.cpp" "CMakeFiles/myproject.dir/src/matrix.cpp.obj" "gcc" "CMakeFiles/myproject.dir/src/matrix.cpp.obj.d"
-  "C:/Users/Ayush Khanal/AI-ML_Framework/src/vector.cpp" "CMakeFiles/myproject.dir/src/vector.cpp.obj" "gcc" "CMakeFiles/myproject.dir/src/vector.cpp.obj.d"
+  "/home/ayush_don/Desktop/AI-ML_Framework/src/Network.cpp" "CMakeFiles/myproject.dir/src/Network.cpp.o" "gcc" "CMakeFiles/myproject.dir/src/Network.cpp.o.d"
+  "/home/ayush_don/Desktop/AI-ML_Framework/src/lin_regression.cpp" "CMakeFiles/myproject.dir/src/lin_regression.cpp.o" "gcc" "CMakeFiles/myproject.dir/src/lin_regression.cpp.o.d"
+  "/home/ayush_don/Desktop/AI-ML_Framework/src/main.cpp" "CMakeFiles/myproject.dir/src/main.cpp.o" "gcc" "CMakeFiles/myproject.dir/src/main.cpp.o.d"
+  "/home/ayush_don/Desktop/AI-ML_Framework/src/matrix.cpp" "CMakeFiles/myproject.dir/src/matrix.cpp.o" "gcc" "CMakeFiles/myproject.dir/src/matrix.cpp.o.d"
+  "/home/ayush_don/Desktop/AI-ML_Framework/src/vector.cpp" "CMakeFiles/myproject.dir/src/vector.cpp.o" "gcc" "CMakeFiles/myproject.dir/src/vector.cpp.o.d"
+  "" "myproject" "gcc" "CMakeFiles/myproject.dir/link.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
